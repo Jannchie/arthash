eb8c15cf827493bbf1ab094ca7c09ca2864199ef
Jianqi Pan <jannchie@gmail.com>
2026-05-16 17:55:38 +0900

    ci(github-actions): add github actions for playground deployment

--- packages/arthash-playground/vite.config.ts
+++ packages/arthash-playground/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+declare const process: { env: Record<string, string | undefined> };
+
 export default defineConfig({
+  base: process.env.BASE_URL ?? "/",
   plugins: [vue()],
   server: {
     port: 7631,
